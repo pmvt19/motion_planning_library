5bb4588937499571edea272d78cf407f7667e381
ptelag2 <ptelag2@illinois.edu>
2025-04-10 15:11:21 -0500

    Added central points and critical points to obstacle sets to be used for fixed tasks

--- obstacle_sets.py
+++ obstacle_sets.py
@@ -6,6 +6,7 @@ class ObstacleSet():
     def __init__(self, obstacles, boundary):
         self.obstacles = obstacles
         self.boundary = boundary
+        self.central_points = []
         self.critical_points = []
 
 class ObstacleSet2d(ObstacleSet):
@@ -38,15 +39,11 @@ class TestSet(ObstacleSet2d):
 
 class ParkingSpace(ObstacleSet2d):
     def __init__(self):
-        self.obstacles = []
-        
-        self.parking_space_samples = []
-        self.parking_space_centers = []
-
+        obstacles = []
         x_range = [-15,15]
         y_range = [-15,15]
-        self.boundary = Polygon([(x_range[0], y_range[0]), (x_range[0], y_range[1]), (x_range[1], y_range[1]), (x_range[1], y_range[0])])
-        self.angular_dims_start = 2
+        boundary = Polygon([(x_range[0], y_range[0]), (x_range[0], y_range[1]), (x_range[1], y_range[1]), (x_range[1], y_range[0])])
+        super().__init__(obstacles=obstacles, boundary=boundary)
 
         self.obstacles.extend(self.create_parking_space(space_width=5))
         self.obstacles.extend(self.create_parking_space(x_loc=-7.5, y_loc=-7.5, space_width=5))
@@ -77,11 +74,11 @@ class ParkingSpace(ObstacleSet2d):
         x_center = (2 * x_loc + space_width + line_width*2) / 2
         y_center = (2 * y_loc + line_height - line_width) / 2
 
-        self.parking_space_centers.append(np.array([x_center, y_center, 0.0]))
-
         sample_radius = 2
-        space_samples = np.array([x_center, y_center, 0]) + (np.random.normal(size=(1000, 3)) * sample_radius)
-        self.parking_space_samples.extend(space_samples)
+        space_samples = np.array([x_center, y_center]) + (np.random.normal(size=(1000, 2)) * sample_radius)
+
+        self.central_points.append(np.array([x_center, y_center]))
+        self.critical_points.extend(space_samples)
 
         return obs
         
