2769f6ce8327bbfc58a7d84bc31e947ccce64aff
ptelag2 <ptelag2@illinois.edu>
2025-04-12 00:04:50 -0500

    generalized edge interpolation

--- utils.py
+++ utils.py
@@ -38,86 +38,6 @@ def create_rectangle_geometry(x_loc, y_loc, x_width, y_length):
                         [x_loc+x_width/2, y_loc-y_length/2],])
     return shape
 
-# To make general
-# def interpolate_edge(start, end, delta):
-#     x1, y1, theta1 = start
-#     x2, y2, theta2 = end
-
-#     edge_length = np.linalg.norm(end - start)
-#     num_checks = int(edge_length / delta)
-#     t_vals = np.linspace(0, 1, num_checks)  # Parameter t ∈ [0,1]
-    
-#     x_vals = (1 - t_vals) * x1 + t_vals * x2
-#     y_vals = (1 - t_vals) * y1 + t_vals * y2
-    
-#     # Spherical interpolation of theta
-#     cos_theta = (1 - t_vals) * np.cos(theta1) + t_vals * np.cos(theta2)
-#     sin_theta = (1 - t_vals) * np.sin(theta1) + t_vals * np.sin(theta2)
-#     theta_vals = np.arctan2(sin_theta, cos_theta)
-
-#     return np.vstack((x_vals, y_vals, theta_vals)).T
-
-# def interpolate_SE2_edge(start, end, delta):
-#     x1, y1, theta1 = start
-#     x2, y2, theta2 = end
-
-#     edge_length = np.linalg.norm(end - start)
-#     num_checks = int(edge_length / delta)
-#     t_vals = np.linspace(0, 1, num_checks+2)  # Parameter t ∈ [0,1]
-    
-#     x_vals = (1 - t_vals) * x1 + t_vals * x2
-#     y_vals = (1 - t_vals) * y1 + t_vals * y2
-    
-#     # Spherical interpolation of theta
-#     cos_theta = (1 - t_vals) * np.cos(theta1) + t_vals * np.cos(theta2)
-#     sin_theta = (1 - t_vals) * np.sin(theta1) + t_vals * np.sin(theta2)
-#     theta_vals = np.arctan2(sin_theta, cos_theta)
-
-#     return np.vstack((x_vals, y_vals, theta_vals)).T
-
-# def interpolate_edge_mobile_arm(start, end, delta):
-#     x1, y1, theta1_1, theta1_2, theta1_3 = start.value
-#     x2, y2, theta2_1, theta2_2, theta2_3 = end.value
-
-#     edge_length = np.linalg.norm(end.value - start.value)
-#     num_checks = int(edge_length / delta)
-#     t_vals = np.linspace(0, 1, num_checks)  # Parameter t ∈ [0,1]
-    
-#     x_vals = (1 - t_vals) * x1 + t_vals * x2
-#     y_vals = (1 - t_vals) * y1 + t_vals * y2
-    
-#     # Spherical interpolation of theta
-#     cos_theta1 = (1 - t_vals) * np.cos(theta1_1) + t_vals * np.cos(theta2_1)
-#     sin_theta1 = (1 - t_vals) * np.sin(theta1_1) + t_vals * np.sin(theta2_1)
-#     theta1_vals = np.arctan2(sin_theta1, cos_theta1)
-
-#     cos_theta2 = (1 - t_vals) * np.cos(theta1_2) + t_vals * np.cos(theta2_2)
-#     sin_theta2 = (1 - t_vals) * np.sin(theta1_2) + t_vals * np.sin(theta2_2)
-#     theta2_vals = np.arctan2(sin_theta2, cos_theta2)
-
-#     cos_theta3 = (1 - t_vals) * np.cos(theta1_3) + t_vals * np.cos(theta2_3)
-#     sin_theta3 = (1 - t_vals) * np.sin(theta1_3) + t_vals * np.sin(theta2_3)
-#     theta3_vals = np.arctan2(sin_theta3, cos_theta3)
-
-#     # return np.vstack((x_vals, y_vals, theta_vals)).T
-#     return np.vstack((x_vals, y_vals, theta1_vals, theta2_vals, theta3_vals)).T
-
-# def interpolate_euclidean_edge(start : np.ndarray, end : np.ndarray, delta):
-#     dir = (end - start) / np.linalg.norm(end - start)
-#     interpolated_points = []
-#     cur_node = start
-
-#     edge_length = np.linalg.norm(end - start)
-#     num_checks = int(edge_length / delta)
-
-#     for i in range(num_checks):
-#         interpolated_points.append(cur_node)
-#         cur_node = cur_node + dir * delta
-    
-#     interpolated_points.append(end)
-
-#     return interpolated_points
-
 def calculate_edge_gradient(start_state : NumpyState, end_state : NumpyState):
     start = start_state.value
     end = end_state.value
@@ -145,6 +65,32 @@ def interpolate_edge(start : NumpyState, end : NumpyState, delta : float):
         angular_dims_start = start.angular_dims_start
         edge_states[:, angular_dims_start:] = edge_states[:, angular_dims_start:] % (2*np.pi)
     return edge_states
+
+
+def batch_calculate_gradient(start_states : np.ndarray, end_states : np.ndarray, angular_dims_start):
+    # start_states: (B, d), # end_states: (B, d)
+    batch_gradient = end_states - start_states
+    if angular_dims_start is not None:
+        batch_gradient[:, angular_dims_start:] = np.arctan2(np.sin(batch_gradient[:, angular_dims_start:]), np.cos(batch_gradient[:, angular_dims_start:]))
+    return batch_gradient
+
+def batch_interpolate_edge(start_states : np.ndarray, end_states : np.ndarray, delta : float, angular_dims_start):
+    # (B, d), # (B, d)
+    B, d  = start_states.shape
+    gradients = (end_states - start_states)
+    gradients = batch_calculate_gradient(start_states, end_states, angular_dims_start)
+    lengths = np.linalg.norm(gradients, axis=1)
+    normalized_gradients = gradients / lengths.reshape(-1, 1) # TODO: Probably need to reshape # (B, d)
+
+    num_steps = np.ceil((lengths / delta) + 1).astype(np.int32)
+    max_steps = np.max(num_steps).astype(np.int32)
+
+    normalized_gradients = normalized_gradients.reshape(-1, 1, d) # Reshape normalized vectors for repeating function
+    edge_states_derivative = np.repeat(normalized_gradients * delta, (max_steps), axis=1) # (B, max_steps, d)
+    edge_states_derivative[:,0,:] = 0
+    edge_states = np.cumsum(edge_states_derivative, axis=1) + start_states.reshape(-1, 1, d) # (B, max_steps, d) + (B,1,d)
+    edge_states[np.arange(B), (num_steps-1), :] = end_states
+    return edge_states, num_steps
 
 def euclidean_distance(start, end):
     return np.linalg.norm(end-start)
@@ -186,3 +132,9 @@ def numpystate_distance(state1, state2):
 #     if not np.all(np.isclose(curr, end)):
 #         pts.append(np.copy(end))
 #     return np.array(pts)
+
+def interpolate_path(path : Path, delta : float):
+    interpolated_path = []
+    for i in range(len(path)-1):
+        interpolated_path.extend(interpolate_edge(path[i], path[i+1], delta))
+    return Path(interpolated_path)
