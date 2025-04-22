3d6aca82c059cd59df33d1540c9d8ba6219c6872
ptelag2 <ptelag2@illinois.edu>
2025-04-22 11:11:39 -0500

    Added console warning if parameters are double specified

--- graph.py
+++ graph.py
@@ -5,12 +5,15 @@ from matplotlib.collections import LineCollection
 from heapq import heappush, heappop
 from collections import defaultdict
 import time
+from utils import issue_warning
 
 class Graph():
     def __init__(self, vertices, num_neighbors=None, edge_dist_radius=None):
         self.vertices = vertices
         self.num_neighbors = num_neighbors
         self.edge_dist_radius = edge_dist_radius
+
+        issue_warning(self.num_neighbors is not None and self.edge_dist_radius is not None, 'Specified both num_neighbors and edge_dist_radius, defaulting to num_neighbors', 'warning')
 
         if self.num_neighbors is not None:
             self.connection_strategy = 'knn'
