7b38d810b2949bfaab242a348ece0ded50f37b1e
ptelag2 <ptelag2@illinois.edu>
2025-04-26 18:14:48 -0500

    Removed old code

--- interactive_robot.py
+++ interactive_robot.py
@@ -4,41 +4,10 @@ import matplotlib.pyplot as plt
 import pygame
 from controller.xbox_controller import XboxController
 from obstacle_sets import ParkingSpace
-# import cv2
-
-# class InteractiveRobot():
-#     def __init__(self):
-#         pass
-
-#     def control(self):
-#         raise NotImplementedError
-
-# def read_inputs():
-#     key = cv2.waitKey(0)
-#     # print(key)
-#     if key == ord('w'):
-#         return [0, 0, 1, 0]
-#     elif key == ord('a'):
-#         return [1, 0, 0, 0]
-#     elif key == ord('s'):
-#         return [0, 0, 0, 1]
-#     elif key == ord('d'):
-#         return [0, 1, 0, 0]
-#     return [0, 0, 0, 0]
-
-# import select
-# import sys
-
-# def non_blocking_input():
-#     i, o, e = select.select([sys.stdin], [], [], 0)
-#     if i:
-#         return sys.stdin.readline().strip()
-#     return None
 
 def get_input(controller):
     controller.update_state()
     state = controller.get_contoller_state()
-    # state = state[6:10]
     return state
 
 if __name__ == '__main__':
@@ -62,13 +31,11 @@ if __name__ == '__main__':
         
         control = env.input_to_control(input)
         state = env.simulate_step(state, control)
-        # print(input, state.value)
 
         plt.clf()
         env.draw_environment(plt.gca())
         env.draw_state(plt.gca(), state)
         plt.pause(0.01)
-        # time.sleep(0.1)
 
         if input[XboxController.XboxControls.LBUMPER]:
             print("Exiting")
