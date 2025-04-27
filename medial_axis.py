1e90a401fd93c042a098b8a8b25901c12fb0b1ef
pmvt19 <praval.telagi@gmail.com>
2025-04-26 19:40:48 -0500

    Removed extra comments from medial_axis file

--- medial_axis.py
+++ medial_axis.py
@@ -105,118 +105,18 @@ if __name__ == '__main__':
     print(f"Setting Seed: {seed}")
     np.random.seed(seed)
     env = PointRobot()
+
     # env = PlanarMobileArm()
     # env.set_obstacles(BiasedPassage(num_walls=2))
     # env.set_obstacles(ParkingSpace())
+
     env.set_obstacles(RandomSamplePassage(gap_width=0.09))
     env = ApproximationSpace(env, do_overapproximation=False)
-    # env.set_obstacles(TestSet())
-    
 
-    # plt.axis('off')
-    # env.draw_environment(plt.gca())
-
-    # fig = plt.gcf()
-    # width, height = fig.canvas.get_width_height()
-    # # plt.savefig("imgs/env.png", bbox_inches='tight', pad_inches=0)
-    # # exit()
-
-    # data_obj = BytesIO()
-    # plt.savefig(data_obj, format='png', bbox_inches='tight', pad_inches=0)
-    # data_obj.seek(0)
-
-    # # img = np.frombuffer()
-    # # img = np.frombuffer(data_obj).reshape(height, width, 3)
-    # # img = plt.imread(data_obj)#.reshape(height, width, 3)
-    # img = Image.open(data_obj).convert("L")
-    # img = np.array(img)
-
-    # # print(data_obj)
-    # # plt.clf()
-    # # plt.imshow(img)
-    # # plt.show()
-    # # exit()
-
-    # env = ApproximationSpace(env, do_overapproximation=False)
-    # # img = cv2.imread("imgs/env.png", cv2.IMREAD_GRAYSCALE)
-    # img[:,0] = False
-    # img[:,-1] = False
-    # img[0,:] = False
-    # img[-1,:] = False
-    # # plt.imshow(img)
-    # # plt.show()
-    # print(type(img), img.shape)
-    # ylen, xlen = img.shape
-
-    # ma_img = medial_axis(img, return_distance=False)
-    # # print(np.unique(ma_img))
-    # plt.imshow(ma_img)
-    # plt.show()
-    # # cv2.imshow('frame', ma_img)
-
-    # # xs, ys = np.where(ma_img == True)
-    # ma_img = ma_img[::-1, :]
-    # ys, xs = np.where(ma_img == True)
-
-    # env.x_range = [0,40]
-    # # env.x_range = [0,30]
-    # env.y_range = [0,10]
-
-    # # env.x_range = [-15,15]
-    # # env.y_range = [-15,15]
-
-    # scaled_ys = ((ys / ylen) * (env.y_range[1] - env.y_range[0]) + env.y_range[0])
-    # scaled_xs = (xs / xlen) * (env.x_range[1] - env.x_range[0]) + env.x_range[0]
-
-    # xs = scaled_xs
-    # ys = scaled_ys
-
-    # ma_coords = np.hstack((xs.reshape(-1,1), ys.reshape(-1,1)))
-
-    # ma_circles = np.hstack((ma_coords, np.zeros((xs.shape[0],1)))).reshape(1, -1, 3)
-    # validities = env.circles_to_indiv_validity(env.obstacle_circles, ma_circles)
-
-    # validities = np.all(validities, axis=2)[0]
-
-    # ma_circles = ma_circles[0][validities]
-
-    # # env.draw_environment(plt.gca())
-    # # env.space.draw_environment(plt.gca())
-    # # plt.scatter(ma_circles[:, 0], ma_circles[:, 1])
-    # # plt.show()
-
-    # ma_points = ma_circles[:, :2]
-    # idxes = np.random.choice(len(ma_points), size=(int(0.1*len(ma_points))))
-    # ma_points = ma_points[idxes]
-
-    # env.draw_environment(plt.gca())
-    # env.space.draw_environment(plt.gca())
-    # plt.scatter(ma_points[:, 0], ma_points[:, 1])
-    # plt.show()
-
-
-    # print(f"Num Points: {len(ma_points)}")
-
-    # starting_points = []
-    # for i in range(len(ma_points)):
-    #     print(f"Point: {i}", end='\r')
-    #     configs = env.space.sample_configs_ee_target(ma_points[i])
-    #     starting_points.append(configs)
-    #     # print(configs.shape)
-    #     # exit()
-
-    #     # if configs.shape[0] > 0:
-    #     #     print("Found Points")
-
-    # prm_starting_configs = np.concatenate(starting_points, axis=0)
-    # print(prm_starting_configs.shape)
-
-    # prm_starting_configs = ma_points
-
-
-    start, target = env.sample_valid_point(), env.sample_valid_point()
-    start, target = env.sample_valid_point(), env.sample_valid_point()
+    # start, target = env.sample_valid_point(), env.sample_valid_point()
+    # start, target = env.sample_valid_point(), env.sample_valid_point()
     print("Start and Target")
+    start = env.make_state(np.array([5.0, 5.0]))
     target = env.make_state(np.array([35.0, 5.0]))
     print(start.value, target.value)
     # print(start.value, target.value)
@@ -229,26 +129,3 @@ if __name__ == '__main__':
     plt.show()
 
     rrt.show_medial_axis()
-
-    # plt.clf()
-    # env.draw_environment(plt.gca())
-    # env.space.draw_environment(plt.gca())
-
-    # for config in prm_starting_configs:
-    #     print(config)
-    #     env.draw_state(plt.gca(), env.make_state(config))
-    # # plt.scatter(ma_points[:, 0], ma_points[:, 1])
-    # plt.show()
-
-    # env.edge_validity_delta = 0.005
-    # prm = PRM(env=env, num_samples=20, num_neighbors=10, validate_edges=True)
-    # prm.create_graph(starting_samples=prm_starting_configs)
-    # start, target = env.sample_valid_point(), env.sample_valid_point()
-
-    # path = prm.search(start, target)
-    # env.draw_environment(plt.gca())
-    # prm.draw(plt.gca())
-    # plt.show()
-
-    # path = interpolate_path(path, env, 0.1)
-    # env.space.animate_path(path, frame_delay=0.001)
