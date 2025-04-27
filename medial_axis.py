ac7b2cd355e7e7874263b500e28266bb5481b307
pmvt19 <praval.telagi@gmail.com>
2025-04-26 19:29:20 -0500

    Added MedialAxis Classes for RRT and PRM which compute the workspace medial axis

--- medial_axis.py
+++ medial_axis.py
@@ -14,24 +14,80 @@ from prm import PRM
 
 from biased_rrt import BiasedSamplingRRT
 from rrt import RRT
+from PIL import Image
 
 # Medial Axis Can Either Guide PRM (Use as starting vertices) or RRT (Biased Sampling)
 
-class MedialAxisRRT(BiasedSamplingRRT):
-    def __init__(self, env, biased_points, points_bias=0.4, delta=0.5):
-        # COMPUTE MEDIAL AXIS POINTS HERE
+def compute_medial_axis_points(env : ApproximationSpace, percent_kept=0.1):
+    plt.axis('off')
+    env.space.draw_environment(plt.gca())
 
-        # MEDIAL AXIS POINTS TO ENV SPECFIC CONFIGURATIONS
+    data_obj = BytesIO()
+    plt.savefig(data_obj, format='png', bbox_inches='tight', pad_inches=0)
+    data_obj.seek(0)
+
+    img = Image.open(data_obj).convert("L")
+    img = np.array(img)
+
+    img[:,0] = False
+    img[:,-1] = False
+    img[0,:] = False
+    img[-1,:] = False
+
+    ylen, xlen = img.shape
+
+    ma_img = medial_axis(img, return_distance=False)
+
+    ma_img = ma_img[::-1, :]
+    ys, xs = np.where(ma_img == True)
+
+    env.x_range = [0,40]
+    env.y_range = [0,10]
+
+    scaled_ys = ((ys / ylen) * (env.y_range[1] - env.y_range[0]) + env.y_range[0])
+    scaled_xs = (xs / xlen) * (env.x_range[1] - env.x_range[0]) + env.x_range[0]
+
+    xs = scaled_xs
+    ys = scaled_ys
+
+    ma_coords = np.hstack((xs.reshape(-1,1), ys.reshape(-1,1)))
+
+    ma_circles = np.hstack((ma_coords, np.zeros((xs.shape[0],1)))).reshape(1, -1, 3)
+    validities = env.circles_to_indiv_validity(env.obstacle_circles, ma_circles)
+
+    validities = np.all(validities, axis=2)[0]
+
+    ma_circles = ma_circles[0][validities]
+
+    ma_points = ma_circles[:, :2]
+    idxes = np.random.choice(len(ma_points), size=(int(percent_kept*len(ma_points))))
+    ma_points = ma_points[idxes]
+
+    return ma_points
+
+class MedialAxisRRT(BiasedSamplingRRT):
+    def __init__(self, env, points_bias=0.4, delta=0.5):
         
-        super().__init__(env=env, biased_points=biased_points, points_bias=points_bias, delta=delta)
+        # COMPUTE MEDIAL AXIS POINTS WORKSPACE HERE
+        ma_points = compute_medial_axis_points(env, percent_kept=0.1)
+        # MEDIAL AXIS POINTS TO ENV SPECFIC CONFIGURATIONS
+
+        configs = env.batch_sample_points_around_target(ma_points) # TODO: IMPLEMENT THIS FUNCTION
+        super().__init__(env=env, biased_points=configs, points_bias=points_bias, delta=delta)
 
 class MedialAxisPRM(PRM):
-    def __init__(self, env):
-        # COMPUTE MEDIAL AXIS POINTS HERE
+    def __init__(self, env, num_samples=1000, num_neighbors=10, edge_dist_radius=None):
+
+        # COMPUTE MEDIAL AXIS POINTS WORKSPACE HERE
+        ma_points = compute_medial_axis_points(env, percent_kept=0.1)
 
         # MEDIAL AXIS POINTS TO ENV SPECFIC CONFIGURATIONS
+        configs = env.batch_sample_points_around_target(ma_points) # TODO: IMPLEMENT THIS FUNCTION
+        
+        super().__init__(env=env, num_samples=num_samples, num_neighbors=num_neighbors, edge_dist_radius=edge_dist_radius, validate_edges=True)
 
-        super().__init__(env=env) # Starting Points
+        print("Creating Graph In Initialization")
+        self.create_graph(starting_samples=configs)
 
 if __name__ == '__main__':
     seed = np.random.randint(0, 100)
@@ -44,24 +100,41 @@ if __name__ == '__main__':
     # env.set_obstacles(ParkingSpace())
     env.set_obstacles(RandomSamplePassage(gap_width=0.09))
     # env.set_obstacles(TestSet())
-    env = ApproximationSpace(env, do_overapproximation=False)
+    
 
-    # plt.axis('off')
-    # env.draw_environment(plt.gca())
+    plt.axis('off')
+    env.draw_environment(plt.gca())
+
+    fig = plt.gcf()
+    width, height = fig.canvas.get_width_height()
     # plt.savefig("imgs/env.png", bbox_inches='tight', pad_inches=0)
     # exit()
 
-    # data_obj = BytesIO()
-    # plt.savefig(data_obj, format='png')
+    data_obj = BytesIO()
+    plt.savefig(data_obj, format='png', bbox_inches='tight', pad_inches=0)
+    data_obj.seek(0)
 
+    # img = np.frombuffer()
+    # img = np.frombuffer(data_obj).reshape(height, width, 3)
+    # img = plt.imread(data_obj)#.reshape(height, width, 3)
+    img = Image.open(data_obj).convert("L")
+    img = np.array(img)
 
-    img = cv2.imread("imgs/env.png", cv2.IMREAD_GRAYSCALE)
+    # print(data_obj)
+    # plt.clf()
+    # plt.imshow(img)
+    # plt.show()
+    # exit()
+
+    env = ApproximationSpace(env, do_overapproximation=False)
+    # img = cv2.imread("imgs/env.png", cv2.IMREAD_GRAYSCALE)
     img[:,0] = False
     img[:,-1] = False
     img[0,:] = False
     img[-1,:] = False
     # plt.imshow(img)
     # plt.show()
+    print(type(img), img.shape)
     ylen, xlen = img.shape
 
     ma_img = medial_axis(img, return_distance=False)
