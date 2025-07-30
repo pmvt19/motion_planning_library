2d6c8ca850ba963850efb98732c2c82e593f459a
pmvt19 <praval.telagi@gmail.com>
2025-07-29 21:49:06 -0700

    Cleaned up some code in circle_approx.py

--- circle_approximation.py
+++ circle_approximation.py
@@ -16,6 +16,7 @@ class ApproximationSpace(RobotSpace):
         self.do_overapproximation = do_overapproximation
         self.batch_size = batch_size
         self.obstacle_circles = self.space_to_circles()
+        
     def dist(self, state1, state2):
         return self.space.dist(state1, state2)
     
@@ -185,19 +186,6 @@ class ApproximationSpace(RobotSpace):
         radii = np.zeros((points.shape[0], 1))
         zero_radius_circles = np.concatenate((points, radii), axis=1)
         return zero_radius_circles
-    
-    # TODO: Combine with circles_to_validity function
-    def circles_to_indiv_validity(self, obstacle_circles, robot_circles):
-        B = robot_circles.shape[0]
-        robot_xy = robot_circles[:, :, :2]
-        obst_xy = obstacle_circles[:, :2]
-        distance_mat = np.sqrt(np.sum(robot_xy**2, axis=2, keepdims=True) + np.sum(obst_xy**2, axis=1, keepdims=True).T + (-2 * (robot_xy @ obst_xy.T)))
-
-        min_dists = robot_circles[:, :, 2].reshape(B, -1, 1) + obstacle_circles[:, 2].reshape(1, 1, -1)
-
-        validity_mask = distance_mat > min_dists
-        # validity_mask = validity_mask.reshape(B, -1)
-        return validity_mask
 
     def circles_to_validity(self, obstacle_circles, robot_circles):
         B = robot_circles.shape[0]
@@ -228,7 +216,6 @@ class ApproximationSpace(RobotSpace):
         return stacked_validities
 
     def draw_state(self, ax, state):
-        start_time = time.time()
         circles = self.states_to_circles(np.array([self.get_state_value(state)]))[0]
         patch_list = [patches.Circle((x,y), r) for (x,y,r) in circles]
         patch_collection = PatchCollection(patch_list, color='red')
@@ -252,18 +239,25 @@ class ApproximationSpace(RobotSpace):
     def make_state(self, state):
         return self.space.make_state(state)
     
+    def batch_sample_points_around_target(self, targets):
+        return self.space.batch_sample_points_around_target(targets)
+    
 if __name__ == "__main__":
     np.random.seed(0)
 
-    env = PolygonalRobot()
-    # env.set_obstacles(TestSet())
-    env.set_obstacles(NonRegularPolygonObst())
-    state = env.make_state(np.array([-1.0,3.0,np.pi/4]))
-    state = env.make_state(np.array([-4.0,3.0,np.pi/4]))
-    # env = ApproximationSpace(env, do_overapproximation=True)
+    # env = PolygonalRobot()
+    env = PlanarMobileArm()
+    env.set_obstacles(TestSet())
+    # env.set_obstacles(NonRegularPolygonObst())
+    # state = env.make_state(np.array([-1.0,3.0,np.pi/4]))
+    # state = env.make_state(np.array([-4.0,3.0,np.pi/4]))
+    # state = env.make_state(np.array([-4.0,3.0,np.pi/4]))
+    state = env.make_state(np.array([-4.0,3.0,np.pi/2,np.pi,np.pi]))
+    env = ApproximationSpace(env, do_overapproximation=True)
     env.draw_environment(plt.gca())
     # env.space.draw_environment(plt.gca())
     env.draw_state(plt.gca(), state)
+    env.space.draw_state(plt.gca(), state)
     # env.space.draw_state(plt.gca(), state)
     plt.show()
 
