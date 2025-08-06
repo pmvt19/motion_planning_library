f6517d9f134371619ec30431c10677416d17d783
pmvt19 <praval.telagi@gmail.com>
2025-08-05 23:53:55 -0700

    Templated features to add to db

--- database.py
+++ database.py
@@ -26,26 +26,42 @@ class Database():
             path_edges = [(path[i].value[:2], path[i+1].value[:2]) for i in range(len(path)-1)]
             ax.add_collection(LineCollection(path_edges, color=cmap(i)))
     
+    def add_path(self, path):
+        raise NotImplementedError
+
+    def merge_dbs(self, other_db):
+        raise NotImplementedError
+    
     def __len__(self):
         return len(self.paths)
+
+    def __add__(self, other_db):
+        raise NotImplementedError
+    
+    def __getitem__(self, idx):
+        raise NotImplementedError
+    
+    def populate_db(self, env, num_envs, num_paths_per_env):
+        raise NotImplementedError
     
 
 if __name__ == '__main__':
 
-    db_save_path = 'saves/database_v5.pickle'
+    # db_save_path = 'saves/database_v6.pickle'
+    db_save_path = 'saves/database_v1_bpe3.pickle'
 
     db = Database()
 
-    for i in range(100):
+    for i in range(20):
         env = PointRobot()
-        env.set_obstacles(BiasedPassage(num_walls=8, bias=0.5))
+        env.set_obstacles(BiasedPassage(num_walls=3, bias=0.5))
         env = ApproximationSpace(env, batch_size=1000, do_overapproximation=True)
 
         # prm = IncrementalPRM(env, num_samples=1000, num_neighbors=20)
         prm = PRM(env, num_samples=5000, num_neighbors=10)
         prm.create_graph()
 
-        for j in range(10):
+        for j in range(20):
             print(f"Env {i+1}, Task {j+1}")
             start, target = env.sample_valid_point(), env.sample_valid_point()
 
@@ -56,4 +72,3 @@ if __name__ == '__main__':
 
 
     db.save_to_path(db_save_path)
-    # pickle.dump(db, open(db_save_path, 'wb'))
