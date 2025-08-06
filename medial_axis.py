b939c72b77bc105bed33f545d54aaaaa561f95b3
pmvt19 <praval.telagi@gmail.com>
2025-08-05 23:57:20 -0700

    Saved more information about medial axis in search methods

--- medial_axis.py
+++ medial_axis.py
@@ -1,6 +1,6 @@
 # from skimage.morphology import medial_axis
 
-from space import PointRobot, PlanarMobileArm
+from space import PointRobot, PlanarMobileArm, PolygonalRobot
 from obstacle_sets import BiasedPassage, RandomSamplePassage, TestSet, ParkingSpace
 import matplotlib.pyplot as plt
 import numpy as np
@@ -9,7 +9,7 @@ from circle_approximation import ApproximationSpace
 
 from io import BytesIO
 from skimage.morphology import medial_axis
-from utils import interpolate_path
+from utils import interpolate_path, smooth_path
 from prm import PRM
 
 from biased_rrt import BiasedSamplingRRT
@@ -76,10 +76,11 @@ class MedialAxisRRT(BiasedSamplingRRT):
         ma_points, ma_img = compute_medial_axis_points(env, percent_kept=0.1)
 
         self.ma_img = ma_img
+        self.ma_points = ma_points
         # MEDIAL AXIS POINTS TO ENV SPECFIC CONFIGURATIONS
 
-        # configs = env.batch_sample_points_around_target(ma_points) # TODO: IMPLEMENT THIS FUNCTION
-        configs = ma_points
+        configs = env.batch_sample_points_around_target(ma_points) # TODO: IMPLEMENT THIS FUNCTION
+        # configs = ma_points
         super().__init__(env=env, biased_points=configs, points_bias=points_bias, delta=delta)
     
     def show_medial_axis(self):
@@ -95,41 +96,62 @@ class MedialAxisPRM(PRM):
         # COMPUTE MEDIAL AXIS POINTS WORKSPACE HERE
         ma_points, ma_img = compute_medial_axis_points(env, percent_kept=0.1)
 
+        self.ma_img = ma_img
+        self.ma_points = ma_points
+
         # MEDIAL AXIS POINTS TO ENV SPECFIC CONFIGURATIONS
         configs = env.batch_sample_points_around_target(ma_points) # TODO: IMPLEMENT THIS FUNCTION
+
+        self.starting_configs = configs
         
         super().__init__(env=env, num_samples=num_samples, num_neighbors=num_neighbors, edge_dist_radius=edge_dist_radius, validate_edges=True)
 
-        print("Creating Graph In Initialization")
-        self.create_graph(starting_samples=configs)
+        # print("Creating Graph In Initialization")
+        # self.create_graph(starting_samples=configs)
+    def create_graph(self, starting_samples=[]):
+        assert (len(starting_samples) == 0), "Starting Samples Doesn't Work Right Now"
+        super().create_graph(starting_samples=self.starting_configs)
 
 if __name__ == '__main__':
     seed = np.random.randint(0, 100)
     seed = 53
     print(f"Setting Seed: {seed}")
     np.random.seed(seed)
-    env = PointRobot()
+    # env = PointRobot()
+    env = PolygonalRobot()
 
     # env = PlanarMobileArm()
     # env.set_obstacles(BiasedPassage(num_walls=2))
     # env.set_obstacles(ParkingSpace())
 
-    env.set_obstacles(RandomSamplePassage(gap_width=0.09))
+    # env.set_obstacles(RandomSamplePassage(gap_width=0.09))
+    env.set_obstacles(RandomSamplePassage(gap_width=1.5))
     env = ApproximationSpace(env, do_overapproximation=False)
 
     # start, target = env.sample_valid_point(), env.sample_valid_point()
     # start, target = env.sample_valid_point(), env.sample_valid_point()
     print("Start and Target")
-    start = env.make_state(np.array([5.0, 5.0]))
-    target = env.make_state(np.array([35.0, 5.0]))
+    start = env.make_state(np.array([5.0, 5.0, 0.0]))
+    target = env.make_state(np.array([35.0, 5.0, 0.0]))
     print(start.value, target.value)
     # print(start.value, target.value)
     # rrt = MedialAxisRRT(env, prm_starting_configs, points_bias=0.7)
-    rrt = MedialAxisRRT(env, points_bias=0.7)
-    # rrt = RRT(env)
-    path = rrt.search(start, target, max_steps=20000, goal_bias=0.1)
-    rrt.draw_tree(plt.gca(), path=path)
-    env.space.draw_environment(plt.gca())
-    plt.show()
+    # rrt = MedialAxisRRT(env, points_bias=0.7)
+    # # rrt = MedialAxisRRT(env, points_bias=0.8)
+    
+    # # rrt = RRT(env)
+    # path = rrt.search(start, target, max_steps=20000, goal_bias=0.1)
+    # rrt.draw_tree(plt.gca(), path=path)
+    # env.space.draw_environment(plt.gca())
+    # plt.show()
 
-    rrt.show_medial_axis()
+    # rrt.show_medial_axis()
+
+    # env.animate_path(path, frame_delay=0.1)
+
+    prm = MedialAxisPRM(env)
+    prm.create_graph()
+    path = prm.search(start, target)
+    path = smooth_path(env, path)
+    path = interpolate_path(path, env, 0.1)
+    env.space.animate_path(path, frame_delay=0.001)
