ade72852ff132ef42d53f87a07c3f4f0e6d76b29
pmvt19 <praval.telagi@gmail.com>
2026-01-04 21:02:33 -0800

    Updating rsg implementation to new environment schema

--- motion_planning/rsg.py
+++ motion_planning/rsg.py
@@ -10,7 +10,8 @@ from matplotlib.collections import LineCollection
 from motion_planning.state import NumpyState
 from motion_planning.utils import smooth_path
 from motion_planning.rrt import RRT 
-from motion_planning.space import PolygonalRobot
+from motion_planning.space import PointRobot, PolygonalRobot, PlanarMobileArm
+from motion_planning.obstacle_sets import BiasedPassage
 
 class RandomSampleGeneration(RRT):
     def __init__(self, env, num_neighbors=5, delta=0.5):
@@ -29,9 +30,9 @@ class RandomSampleGeneration(RRT):
             noise = np.random.normal(size=(self.num_neighbors, node.value.shape[0]))
             noise = (noise / np.linalg.norm(noise, axis=1, keepdims=True)) * self.delta
             new_node_candidates = node.value + noise
-            
-            dists = [self.env.dist(new_node_candidates[i], sampled_point.value) if (self.env.is_valid(new_node_candidates[i]) and self.env.is_valid_edge(node, self.env.make_state(new_node_candidates[i]))) else float('inf') for i in range(self.num_neighbors)]
-            
+
+            dists = [self.env.dist(self.env.make_state(new_node_candidates[i]), sampled_point) if (self.env.is_valid(new_node_candidates[i]) and self.env.is_valid_edge(node, self.env.make_state(new_node_candidates[i]))) else float('inf') for i in range(self.num_neighbors)]
+
             if min(dists) == float('inf'):
                 new_node = node
             else:
@@ -45,30 +46,18 @@ class RandomSampleGeneration(RRT):
         return new_node
 
 if __name__ == "__main__":
-    # seed = np.random.randint(0, 100)
+    seed = np.random.randint(0, 10000)
     seed = 54
     print(f"Setting Seed: {seed}")
     np.random.seed(seed)
-    # start = (0, 0)
-    # target = (9, 9)
-    
-    # env = Environment2d()
-    # env = RandomSamplePassage()
-    env = PolygonalRobot()
+
+    env = PointRobot()
+    env.set_obstacles(BiasedPassage())
     start, target = env.sample_task()
 
-    # start = env.make_state(np.array([0,0]))
-    # target = env.make_state(np.array([9,9]))
-
-    max_steps = 10
-    goal_bias = 0.1
-
     rsg = RandomSampleGeneration(env)
-    # rsg = RRT(env)
-    path = rsg.search(start, target, max_steps=1000, goal_bias=0.1)
-    rsg.draw_tree(plt.gca(), path=path, hold=True)
-    
-    smoothed_path = smooth_path(env, path)
-    rsg.draw_tree(plt.gca(), path=smoothed_path, hold=True)
 
-    # rrt.draw_voronoi_diagram()
+    path = rsg.search(start, target, max_steps=1000, goal_bias=0.1)
+    rsg.draw_tree(plt.gca(), path=path)
+    plt.show()
+
