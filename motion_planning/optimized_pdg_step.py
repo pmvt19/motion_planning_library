6245dfef0826a558d6de4e450f77314b43a2a7b7
pmvt19 <praval.telagi@gmail.com>
2026-04-08 21:55:06 -0700

    Trying different databases

--- motion_planning/optimized_pdg_step.py
+++ motion_planning/optimized_pdg_step.py
@@ -467,7 +467,9 @@ if __name__ == '__main__':
     # db_save_path = "saves/database_bpe8_large.pickle"
     # db_save_path = 'saves/database_v1_bpe3.pickle'
     # db_save_path = 'saves/clustered_database_large_bpe_subsampled.pickle'
-    db_save_path = 'saves/clustered_database_large_bpe_mp_sampler.pickle'
+    # db_save_path = 'saves/clustered_database_large_bpe_mp_sampler.pickle'
+    # db_save_path = 'saves/clustered_database_large_bpe_subsampled.pickle'
+    db_save_path = 'saves/smoothed_interpolated_database_large_bpe_subsampled.pickle'
     
     env = PointRobot()
     env.set_obstacles(BiasedPassage(bias=0.5, num_walls=3))
