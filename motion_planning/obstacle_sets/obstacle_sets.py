7fdeeaf812c4a9c1eadef81e8fefff0fbec3106e
pmvt19 <praval.telagi@gmail.com>
2026-04-27 00:11:41 -0700

    Removing unused imports in obstacle_sets.py

--- motion_planning/obstacle_sets/obstacle_sets.py
+++ motion_planning/obstacle_sets/obstacle_sets.py
@@ -1,10 +1,3 @@
-import numpy as np
-import matplotlib.pyplot as plt
-from shapely import Polygon
-
-from motion_planning.utils import create_rectangle_geometry
-
-
 class ObstacleSet():
     def __init__(self, obstacles, boundary):
         self.obstacles = obstacles
