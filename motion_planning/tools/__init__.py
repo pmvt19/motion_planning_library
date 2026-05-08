173c3e988029b2b2615ac35efe8805724c06631e
pmvt19 <praval.telagi@gmail.com>
2026-05-07 18:19:33 -0700

    Working on adding KinodynamicPath to the tools API

--- motion_planning/tools/__init__.py
+++ motion_planning/tools/__init__.py
@@ -1,10 +1,11 @@
 from .graph import Graph
-from .path import Path
+from .path import Path, KinodynamicPath
 from .state import NumpyState, AngularNumpyState
 
 __all__ = [
     Graph,
     Path,
+    KinodynamicPath,
     NumpyState,
     AngularNumpyState
 ]
