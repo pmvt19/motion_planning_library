4ddfe5570e04164aa82cebf7a974ed747032adac
pmvt19 <praval.telagi@gmail.com>
2026-05-09 16:06:38 -0700

    Converting test file to use unittest

--- motion_planning/tests/unit_tests/test_graph.py
+++ motion_planning/tests/unit_tests/test_graph.py
@@ -1,27 +1,31 @@
+import unittest
+
 import numpy as np
-import matplotlib.pyplot as plt
 
 from motion_planning.tools import Graph
 
-def test_graph_num_nodes_generated():
-    np.random.seed(0)
-    vertices = np.random.uniform(-10, 10, size=(1000, 2))
 
-    graph = Graph(vertices)
-    graph.connect_edges()
+class TestGraph(unittest.TestCase):
 
-    assert(len(graph.vertices) == 1000)
-    assert(len(graph.edges) > 0)
-    assert(graph.connection_strategy == 'knn')
+    def test_graph_num_nodes_generated(self):
+        np.random.seed(0)
+        vertices = np.random.uniform(-10, 10, size=(1000, 2))
 
-def test_graph_edge_connection_strategy_hierarchy():
-    vertices = np.random.uniform(-10, 10, size=(1000, 2))
+        graph = Graph(vertices, num_neighbors=10)
+        graph.connect_edges()
 
-    graph = Graph(vertices, num_neighbors=10)
-    assert(graph.connection_strategy == 'knn')
+        self.assertEqual(len(graph.vertices), 1000)
+        self.assertGreater(len(graph.edges), 0)
+        self.assertEqual(graph.connection_strategy, 'knn')
 
-    graph = Graph(vertices, edge_dist_radius=3)
-    assert(graph.connection_strategy == 'r_neighborhood')
+    def test_graph_edge_connection_strategy_hierarchy(self):
+        vertices = np.random.uniform(-10, 10, size=(1000, 2))
 
-    graph = Graph(vertices, num_neighbors=10, edge_dist_radius=3)
-    assert(graph.connection_strategy == 'knn')
+        graph = Graph(vertices, num_neighbors=10)
+        self.assertEqual(graph.connection_strategy, 'knn')
+
+        graph = Graph(vertices, edge_dist_radius=3)
+        self.assertEqual(graph.connection_strategy, 'r_neighborhood')
+
+        graph = Graph(vertices, num_neighbors=10, edge_dist_radius=3)
+        self.assertEqual(graph.connection_strategy, 'knn')
