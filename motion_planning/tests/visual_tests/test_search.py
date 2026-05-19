d6930c19dbcb3b8537d7d5975bbc0dcb9fe43296
pmvt19 <praval.telagi@gmail.com>
2026-05-18 19:16:17 -0700

    Linting test search file

--- motion_planning/tests/visual_tests/test_search.py
+++ motion_planning/tests/visual_tests/test_search.py
@@ -1,12 +1,22 @@
 import unittest
 
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
 
-from motion_planning.tools import NumpyState
-from motion_planning.space import RobotSpace, PointRobot, ApproximationSpace
-from motion_planning.search import RRT, BiDirectionalRRT, RRTStar, MedialAxisRRT, PRM, LazyPRM, IncrementalPRM, NonUniformPRM, MedialAxisPRM
 from motion_planning.obstacle_sets import BiasedPassage
+from motion_planning.search import (
+    PRM,
+    RRT,
+    BiDirectionalRRT,
+    IncrementalPRM,
+    LazyPRM,
+    MedialAxisPRM,
+    MedialAxisRRT,
+    NonUniformPRM,
+    RRTStar,
+)
+from motion_planning.space import ApproximationSpace, PointRobot, RobotSpace
+from motion_planning.tools import NumpyState
 
 
 class TestRRTSearchMethods(unittest.TestCase):
@@ -17,7 +27,8 @@ class TestRRTSearchMethods(unittest.TestCase):
         self.env.set_obstacles(BiasedPassage(num_walls=1, bias=0.5))
         
         # Manually Define the Task
-        self.start, self.target = self.env.make_state(np.array([5.0, 5.0])), self.env.make_state(np.array([15.0, 5.0]))
+        self.start = self.env.make_state(np.array([5.0, 5.0]))
+        self.target = self.env.make_state(np.array([15.0, 5.0]))
 
     def test_rrt(self):
         rrt = RRT(self.env)
@@ -59,7 +70,8 @@ class TestPRMSearchMethods(unittest.TestCase):
         self.env.set_obstacles(BiasedPassage(num_walls=1, bias=0.5))
         
         # Manually Define the Task
-        self.start, self.target = self.env.make_state(np.array([5.0, 5.0])), self.env.make_state(np.array([15.0, 5.0]))
+        self.start = self.env.make_state(np.array([5.0, 5.0]))
+        self.target = self.env.make_state(np.array([15.0, 5.0]))
     
     def test_prm(self):
         prm = PRM(env=self.env, num_samples=5000, num_neighbors=10, validate_edges=True)
@@ -127,31 +139,3 @@ def test_pdg(env: RobotSpace, start: NumpyState, target: NumpyState):
 
 def test_bidirectional_pdg(env: RobotSpace, start: NumpyState, target: NumpyState):
     pass
-
-# if __name__ == '__main__':
-#     # Create the Environment
-#     env = PointRobot()
-#     env.set_obstacles(BiasedPassage(num_walls=1, bias=0.5))
-    
-#     # Manually Define the Task
-#     start, target = env.make_state(np.array([5.0, 5.0])), env.make_state(np.array([15.0, 5.0]))
-
-#     ## Run Visual Search Tests
-
-#     # RRTs
-#     test_rrt(env, start, target)
-#     test_bidirectional_rrt(env, start, target)
-#     test_rrt_start(env, start, target)
-#     test_medial_axis_rrt(env, start, target)
-
-#     # PRMs
-#     test_prm(env, start, target)
-#     test_lazy_prm(env, start, target)
-#     test_incremental_prm(env, start, target)
-#     test_nonuniform_prm(env, start, target)
-#     test_medial_axis_prm(env, start, target)
-
-#     # Database Methods
-#     test_lightning(env, start, target)
-#     test_pdg(env, start, target)
-#     test_bidirectional_pdg(env, start, target)
