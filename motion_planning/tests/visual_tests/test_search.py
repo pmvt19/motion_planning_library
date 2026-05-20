9e58ee9455a93b4362c9800a36ee794a8c23e3fc
pmvt19 <praval.telagi@gmail.com>
2026-05-19 19:34:13 -0700

    Adding database tests

--- motion_planning/tests/visual_tests/test_search.py
+++ motion_planning/tests/visual_tests/test_search.py
@@ -1,3 +1,4 @@
+import os
 import unittest
 
 import matplotlib.pyplot as plt
@@ -14,9 +15,14 @@ from motion_planning.search import (
     MedialAxisRRT,
     NonUniformPRM,
     RRTStar,
+    Lightning,
+    OptimizedPDG,
+    OptimizedBiDirectionalPDG
 )
 from motion_planning.space import ApproximationSpace, PointRobot, RobotSpace
-
+from motion_planning.database import Database
+from motion_planning.database.db_annotator import populate_db
+from motion_planning.experiments.utils.mp_sampler import MPSampler
 
 class TestRRTSearchMethods(unittest.TestCase):
     def setUp(self):
@@ -71,6 +77,11 @@ class TestPRMSearchMethods(unittest.TestCase):
         # Manually Define the Task
         self.start = self.env.make_state(np.array([5.0, 5.0]))
         self.target = self.env.make_state(np.array([15.0, 5.0]))
+    
+    def visualize_search(self, prm: PRM, path):
+        self.env.draw_environment(plt.gca())
+        prm.draw(plt.gca(), path=path, show_task=True, plot_invalid_edges=False)
+        plt.show()
 
     def test_prm(self):
         prm = PRM(env=self.env, num_samples=5000, num_neighbors=10, validate_edges=True)
@@ -78,10 +89,7 @@ class TestPRMSearchMethods(unittest.TestCase):
 
         path = prm.search(self.start, self.target)
 
-        # Visualize Output
-        self.env.draw_environment(plt.gca())
-        prm.draw(plt.gca(), path=path, show_task=True, plot_invalid_edges=False)
-        plt.show()
+        self.visualize_search(prm, path)
 
     def test_lazy_prm(self):
         prm = LazyPRM(env=self.env, num_samples=5000, num_neighbors=10)
@@ -89,10 +97,7 @@ class TestPRMSearchMethods(unittest.TestCase):
 
         path = prm.search(self.start, self.target)
 
-        # Visualize Output
-        self.env.draw_environment(plt.gca())
-        prm.draw(plt.gca(), path=path, show_task=True, plot_invalid_edges=False)
-        plt.show()
+        self.visualize_search(prm, path)
 
     def test_incremental_prm(self):
         prm = IncrementalPRM(env=self.env, num_samples=1000, num_neighbors=5)
@@ -100,10 +105,7 @@ class TestPRMSearchMethods(unittest.TestCase):
 
         path = prm.search(self.start, self.target)
 
-        # Visualize Output
-        self.env.draw_environment(plt.gca())
-        prm.draw(plt.gca(), path=path, show_task=True, plot_invalid_edges=False)
-        plt.show()
+        self.visualize_search(prm, path)
 
     def test_nonuniform_prm(self):
         prm = NonUniformPRM(env=self.env, num_samples=1000, num_neighbors=5)
@@ -111,10 +113,7 @@ class TestPRMSearchMethods(unittest.TestCase):
 
         path = prm.search(self.start, self.target)
 
-        # Visualize Output
-        self.env.draw_environment(plt.gca())
-        prm.draw(plt.gca(), path=path, show_task=True, plot_invalid_edges=False)
-        plt.show()
+        self.visualize_search(prm, path)
 
     def test_medial_axis_prm(self):
         local_env = ApproximationSpace(self.env, do_overapproximation=True)
@@ -123,22 +122,55 @@ class TestPRMSearchMethods(unittest.TestCase):
 
         path = prm.search(self.start, self.target)
 
-        # Visualize Output
-        self.env.draw_environment(plt.gca())
-        prm.draw(plt.gca(), path=path, show_task=True, plot_invalid_edges=False)
-        plt.show()
+        self.visualize_search(prm, path)
 
 
 class TestDatabaseSearchMethods(unittest.TestCase):
 
     def setUp(self):
-        pass
+        self.create_database()
+
+        # Create the Environment
+        self.env = PointRobot()
+        self.env.set_obstacles(BiasedPassage(num_walls=1, bias=0.5))
+
+        # Manually Define the Task
+        self.start = self.env.make_state(np.array([5.0, 5.0]))
+        self.target = self.env.make_state(np.array([15.0, 5.0]))
+
+    def create_database(self):
+        self.db_path = "saves/tests/test_database_search_method_db.pickle"
+
+        if not os.path.exists(self.db_path):
+            db = Database()
+            mp_sampler = MPSampler(PointRobot(), BiasedPassage, {"num_walls": 3, "bias": 0.5})
+            populate_db(db, mp_sampler, num_envs=5, num_tasks_per_env=10, smooth_paths=False)
+            db.save_to_path(self.db_path)
+
+            # Ensure Created Database is Non-Empty
+            self.assertGreater(len(db), 0)
 
     def test_lightning(self):
-        pass
+        lightning = Lightning(env=self.env, db_path=self.db_path)
+        path = lightning.search(self.start, self.target)
+
+        self.env.draw_environment(plt.gca())
+        lightning.draw(plt.gca(), path=path, show_task=True)
+        plt.show()
 
     def test_pdg(self):
-        pass
+        pdg = OptimizedPDG(env=self.env, db_path=self.db_path)
+        pdg.compute_retained_paths(self.target)
+        path = pdg.search(self.start, self.target)
+
+        self.env.draw_environment(plt.gca())
+        pdg.draw_tree(plt.gca(), path=path)
+        plt.show()
 
     def test_bidirectional_pdg(self):
-        pass
+        pdg = OptimizedBiDirectionalPDG(env=self.env, db_path=self.db_path)
+        path = pdg.search(self.start, self.target)
+
+        self.env.draw_environment(plt.gca())
+        pdg.draw_tree(plt.gca(), path=path)
+        plt.show()
