fee78919c3f1db7f9d21528686bd8f4251713f70
pmvt19 <praval.telagi@gmail.com>
2026-05-19 20:16:59 -0700

    Templating unittests for database

---
+++ motion_planning/tests/unit_tests/test_clustered_database.py
@@ -0,0 +1,17 @@
+import unittest
+
+from motion_planning.database import ClusteredDatabase
+
+
+class TestClusteredDatabase(unittest.TestCase):
+    def test_add_path_with_clusters(self):
+        pass
+
+    def test_merge_databases_with_clusters(self):
+        pass
+
+    def test_clustered_database_subsampling(self):
+        pass
+
+    def test_erase_clustering(self):
+        pass
---
+++ motion_planning/tests/unit_tests/test_database.py
@@ -0,0 +1,16 @@
+import unittest
+
+from motion_planning.database import Database
+
+class TestDatabase(unittest.TestCase):
+    def test_adding_path_to_database(self):
+        pass
+
+    def test_database_returns_correct_length(self):
+        pass
+
+    def test_database_indexing(self):
+        pass
+
+    def test_batch_adding_paths_to_database(self):
+        pass
