08c81dcec119eb5b2ec65ec85a31f509b9fa9e41
pmvt19 <praval.telagi@gmail.com>
2026-05-19 19:18:52 -0700

    Fixing pdg package imports

--- motion_planning/search/__init__.py
+++ motion_planning/search/__init__.py
@@ -10,7 +10,6 @@ from .pdg import PDG, BiDirectionalPDG
 from .biased_rrt import BiasedSamplingRRT
 from .medial_axis import MedialAxisRRT, MedialAxisPRM
 from .lightning import Lightning
-from .pdg import PDG
 from .optimized_pdg import OptimizedPDG as LegacyOptimizedPDG
-from .optimized_pdg_step import OptimizedPDG
+from .optimized_pdg_step import OptimizedPDG, BiDirectionalPDG as OptimizedBiDirectionalPDG
 from .informed_rrt_star import InformedRRTStar
