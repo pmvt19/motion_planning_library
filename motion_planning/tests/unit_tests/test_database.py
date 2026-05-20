60e2b3d8f5a24938a5aea98200626cc71640cf13
pmvt19 <praval.telagi@gmail.com>
2026-05-20 16:50:05 -0700

    Adding unit tests for database

--- motion_planning/tests/unit_tests/test_database.py
+++ motion_planning/tests/unit_tests/test_database.py
@@ -1,9 +1,24 @@
 import unittest
 
 from motion_planning.database import Database
+from motion_planning.tools import Path
+
 
 class TestDatabase(unittest.TestCase):
     def test_adding_path_to_database(self):
+        self.db = Database()
+
+        original_database_length = len(self.db)
+
+        path = Path([])  # Create an Empty Path
+        self.db.add_path(path)
+
+        new_database_length = len(self.db)
+
+        # Original Database should be 1 element smaller than the new database size
+        self.assertEqual(original_database_length + 1, new_database_length)
+
+    def test_database_only_accepts_path_objects(self):
         pass
 
     def test_database_returns_correct_length(self):
