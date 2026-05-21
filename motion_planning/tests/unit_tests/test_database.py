e7e136226d5eff4e7698a5d02ba9a5bdb3b29fdb
pmvt19 <praval.telagi@gmail.com>
2026-05-20 18:52:53 -0700

    Building out database test suite

--- motion_planning/tests/unit_tests/test_database.py
+++ motion_planning/tests/unit_tests/test_database.py
@@ -1,3 +1,4 @@
+import random
 import unittest
 
 from motion_planning.database import Database
@@ -19,13 +20,48 @@ class TestDatabase(unittest.TestCase):
         self.assertEqual(original_database_length + 1, new_database_length)
 
     def test_database_only_accepts_path_objects(self):
-        pass
+        self.db = Database()
+
+        # Cannot add a non Path object to the database
+        with self.assertRaises(AssertionError):
+            self.db.add_path([])
 
     def test_database_returns_correct_length(self):
-        pass
+        self.db = Database()
+
+        # Randomly generate number of paths to create
+        num_paths = random.randint(1, 500)
+
+        for _ in range(num_paths):
+            self.db.add_path(Path([]))
+        
+        self.assertEqual(num_paths, len(self.db))
 
     def test_database_indexing(self):
-        pass
+        self.db = Database()
+
+        path0 = Path([])
+        path1 = Path([])
+
+        self.db.add_path(path0)
+        self.db.add_path(path1)
+
+        # Check DB Indexing refers to correct path objects
+        self.assertTrue(self.db[0] is path0)
+        self.assertTrue(self.db[1] is path1)
+
+        # Check DB Indexing does not refer to correct incorrect path objects
+        self.assertFalse(self.db[0] is path1)
+        self.assertFalse(self.db[1] is path0)
 
     def test_batch_adding_paths_to_database(self):
-        pass
+        self.db = Database()
+
+        original_db_size = len(self.db)
+
+        batch_num_paths = random.randint(1, 500)
+        batch_of_paths = [Path([]) for _ in range(batch_num_paths)]
+
+        self.db.batch_add_paths(batch_of_paths)
+
+        self.assertEqual(original_db_size + batch_num_paths, len(self.db))
