30f585216e4d66794286867ac28df01eae1b9635
pmvt19 <praval.telagi@gmail.com>
2026-05-21 18:19:41 -0700

    Reformating holonomic robot file

--- motion_planning/space/holonomic/holonomic_robot.py
+++ motion_planning/space/holonomic/holonomic_robot.py
@@ -1,13 +1,14 @@
 from motion_planning.space import RobotSpace
 
+
 class HolonomicRobot(RobotSpace):
     def __init__(self):
         super().__init__()
-    
-    def sample_task(self, task_type='random'):
-        if task_type == 'random':
+
+    def sample_task(self, task_type="random"):
+        if task_type == "random":
             start = self.sample_valid_point()
             target = self.sample_valid_point()
-        elif task_type == 'structured':
+        elif task_type == "structured":
             raise NotImplementedError
         return start, target
