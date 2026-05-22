484a1813db1b6e93b1e384241c97dc16c68869ac
pmvt19 <praval.telagi@gmail.com>
2026-05-21 17:41:50 -0700

    Adding interactive element for 3d cspaces

--- motion_planning/visualizations/viz_cspace_3d.py
+++ motion_planning/visualizations/viz_cspace_3d.py
@@ -1,8 +1,11 @@
 import time
 
 import matplotlib.pyplot as plt
+import pygame
 import rerun as rr
+from matplotlib.widgets import Slider
 
+from motion_planning.controller.xbox_controller import XboxController
 from motion_planning.obstacle_sets import RandomSamplePassage
 from motion_planning.space import PolygonalRobot
 from motion_planning.visualizations.viz_cspace_2d import CSpaceVisualizer
@@ -38,11 +41,98 @@ class PolygonalRobotCSpaceVisualizer(CSpaceVisualizer3D):
         self._generate_obstacle_points()
 
     def run_interactive_space(self, tick_delay=0.01):
-        # On the matplotlib side, you should be able to change the parameters 
-        # of the polygonal robot and recompute the obstacle points
-        raise NotImplementedError
+        pygame.init()
+        joysticks = []
+        for i in range(0, pygame.joystick.get_count()):
+            joysticks.append(pygame.joystick.Joystick(i))
+            joysticks[-1].init()
+
+        controller = XboxController(pygame)
+
+        state = self.env.sample_valid_point()
+
+        running = True
+
+        fig, axs = plt.subplots(1, 1)
+        fig.subplots_adjust(left=0.25, bottom=0.25)
+
+        axlink1 = fig.add_axes([0.25, 0.1, 0.65, 0.03])
+        link1_slider = Slider(
+            ax=axlink1,
+            label="Robot Width",
+            valmin=0.1,
+            valmax=5,
+            valinit=0.5,
+            dragging=True,
+        )
+
+        axlink2 = fig.add_axes([0.1, 0.25, 0.0225, 0.63])
+        link2_slider = Slider(
+            ax=axlink2,
+            label="Robot Height",
+            valmin=0.1,
+            valmax=7,
+            valinit=3,
+            orientation="vertical",
+            dragging=True,
+        )
+
+        def update_robot(val):
+            self.env.robot_width = link1_slider.val
+            self.env.robot_length = link2_slider.val
+            self._generate_obstacle_points()
+
+        link1_slider.on_changed(update_robot)
+        link2_slider.on_changed(update_robot)
+
+        # Disable events initially
+        link1_slider.eventson = False
+        link2_slider.eventson = False
+
+        # On mouse release, trigger the event manually
+        def on_release(event):
+            if event.inaxes == link1_slider.ax:
+                link1_slider.eventson = True  # Enable
+                link1_slider._observers.process(
+                    "changed", link1_slider.val
+                )  # Manually trigger callbacks
+                link1_slider.eventson = False  # Disable again immediately
+            if event.inaxes == link2_slider.ax:
+                link1_slider.eventson = True  # Enable
+                link1_slider._observers.process(
+                    "changed", link2_slider.val
+                )  # Manually trigger callbacks
+                link1_slider.eventson = False  # Disable again immediately
+
+        fig.canvas.mpl_connect("button_release_event", on_release)
+
+        rr.init("3D Configuration Space", spawn=True)
+        start_time = time.time()
+        while running:
+            controller.update_state()
+            controller_state = controller.get_contoller_state()
+            x_dot = self.env.input_to_x_dot(controller_state)
+            state = self.env.make_state(state.value + x_dot)
+
+            # Plot Configuration Space (CSpace)
+            rr.set_time("Time", duration=time.time() - start_time)
+            rr.log(
+                "Obstacle Points", rr.Points3D(self.obstacle_points, colors=[0, 0, 255])
+            )
+            rr.log("Robot Location", rr.Points3D(state.value, colors=[255, 0, 0]))
+
+            axs.cla()
+
+            self.env.draw_environment(axs)
+            self.env.draw_state(axs, state)
+            axs.set_title("Workspace")
+            plt.pause(tick_delay)
+
+            if controller_state[XboxController.XboxControls.LBUMPER]:
+                running = False
 
 
 if __name__ == "__main__":
     visualizer = PolygonalRobotCSpaceVisualizer()
-    visualizer.run_visualized_search()
+    # visualizer.run_visualized_search()
+    visualizer.run_interactive_space()
