ce8d752d92b43dc4e0a1344d387c044cbef0e465
pmvt19 <praval.telagi@gmail.com>
2026-05-21 19:04:25 -0700

    Reformatting skid steer car file

--- motion_planning/space/nonholonomic/skid_steer_car.py
+++ motion_planning/space/nonholonomic/skid_steer_car.py
@@ -1,11 +1,10 @@
 import numpy as np
-
 from shapely import affinity
 
-from motion_planning.space import NonHolonomicRobot
-from motion_planning.tools import NumpyState, AngularNumpyState
-from motion_planning.utils import numpystate_distance, create_rectangle_geometry
 from motion_planning.controller.xbox_controller import XboxController
+from motion_planning.space import NonHolonomicRobot
+from motion_planning.tools import AngularNumpyState, NumpyState
+from motion_planning.utils import create_rectangle_geometry, numpystate_distance
 
 
 class SkidSteerCar(NonHolonomicRobot):
@@ -21,7 +20,7 @@ class SkidSteerCar(NonHolonomicRobot):
 
         self.x_range = [-10, 10]
         self.y_range = [-10, 10]
-        self.theta_range = [0, 2*np.pi]
+        self.theta_range = [0, 2 * np.pi]
 
         self.velocity_range = [-3, 3]
         self.delta_range = [-np.pi, np.pi]
@@ -34,10 +33,14 @@ class SkidSteerCar(NonHolonomicRobot):
         y = np.random.uniform(low=self.y_range[0], high=self.y_range[1])
         theta = np.random.uniform(low=self.theta_range[0], high=self.theta_range[1])
         return self.make_state(np.array([x, y, theta]))
-    
+
     def sample_controls(self, bias=0.5):
-        v = np.random.uniform(low=self.velocity_range[0], high=self.velocity_range[1]) # Sample Velocity Uniformly Between: (-3, 3)
-        delta = np.random.uniform(low=self.delta_range[0], high=self.delta_range[1]) # Sample Delta Uniformly Between: (-pi, pi)
+        v = np.random.uniform(
+            low=self.velocity_range[0], high=self.velocity_range[1]
+        )  # Sample Velocity Uniformly Between: (-3, 3)
+        delta = np.random.uniform(
+            low=self.delta_range[0], high=self.delta_range[1]
+        )  # Sample Delta Uniformly Between: (-pi, pi)
 
         # Can only Move Forward OR Turn in place Not Both!
         if np.random.random() < bias:
@@ -46,23 +49,25 @@ class SkidSteerCar(NonHolonomicRobot):
             v = 0.0
         return self.make_control(np.array([v, delta]))
 
-    def make_state(self, state : np.ndarray):
+    def make_state(self, state: np.ndarray):
         return AngularNumpyState(state, angular_dims_start=self.angular_dim_start)
 
-    def make_control(self, control : np.ndarray):
+    def make_control(self, control: np.ndarray):
         return NumpyState(control)
-    
-    def clip_state(self, state : np.ndarray):
-        state = np.clip(state, 
-                        np.array([self.x_range[0], self.y_range[0], -np.inf]), 
-                        np.array([self.x_range[1], self.y_range[1], np.inf]))
+
+    def clip_state(self, state: np.ndarray):
+        state = np.clip(
+            state,
+            np.array([self.x_range[0], self.y_range[0], -np.inf]),
+            np.array([self.x_range[1], self.y_range[1], np.inf]),
+        )
         return state
 
     def dist(self, state1, state2):
         state1 = self.get_state_value(state1)
         state2 = self.get_state_value(state2)
         return numpystate_distance(self.make_state(state1), self.make_state(state2))
-    
+
     def is_valid(self, state):
         self.num_collision_checks += 1
         robot = self.generate_robot_representation(state)
@@ -70,11 +75,13 @@ class SkidSteerCar(NonHolonomicRobot):
             if obs.intersects(robot):
                 return False
         return True
-    
+
     def generate_robot_representation(self, state):
         x, y, theta = self.get_state_value(state)
-        theta -= np.pi/2
-        robot = create_rectangle_geometry(x_loc=x, y_loc=y, x_width=self.car_width, y_length=self.car_length)
+        theta -= np.pi / 2
+        robot = create_rectangle_geometry(
+            x_loc=x, y_loc=y, x_width=self.car_width, y_length=self.car_length
+        )
         robot = affinity.rotate(robot, theta, use_radians=True)
         return robot
 
@@ -84,26 +91,28 @@ class SkidSteerCar(NonHolonomicRobot):
 
     def draw_state(self, ax, state):
         robot, cosmetics = self.generate_costmetic_robot_representation(state)
-        x,y = robot.exterior.xy
-        ax.plot(x,y, color='red')
+        x, y = robot.exterior.xy
+        ax.plot(x, y, color="red")
 
         for c in cosmetics:
-            x,y = c.exterior.xy
-            ax.plot(x,y, color='black')
-    
+            x, y = c.exterior.xy
+            ax.plot(x, y, color="black")
+
     def state_derivative(self, state, control):
         x, y, theta = self.get_state_value(state)
         v, delta = self.get_state_value(control)
-        x_dot = np.array([
-                    v * np.cos(theta) * self.dt,
-                    v * np.sin(theta) * self.dt,
-                    delta * self.dt,
-                ])
+        x_dot = np.array(
+            [
+                v * np.cos(theta) * self.dt,
+                v * np.sin(theta) * self.dt,
+                delta * self.dt,
+            ]
+        )
         return x_dot
 
     def input_to_control(self, inputs):
         # Inputs : [0, 0, 0, 0]
-        # left, right, up, down = 
+        # left, right, up, down =
         # down, left, right, up = inputs
         left = inputs[XboxController.XboxControls.X]
         right = inputs[XboxController.XboxControls.B]
@@ -118,7 +127,7 @@ class SkidSteerCar(NonHolonomicRobot):
         if down:
             return self.make_control(np.array([self.velocity_range[1], 0.0]))
         return self.make_control(np.array([0.0, 0.0]))
-    
+
 
 if __name__ == "__main__":
     import matplotlib.pyplot as plt
