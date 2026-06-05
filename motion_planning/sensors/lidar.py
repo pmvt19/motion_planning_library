fa9b282cd0bdb09f99a9803d6f4574837ccad390
pmvt19 <praval.telagi@gmail.com>
2026-06-04 18:36:14 -0700

    Removing comments

--- motion_planning/sensors/lidar.py
+++ motion_planning/sensors/lidar.py
@@ -74,7 +74,6 @@ class Lidar():
 if __name__ == '__main__':
     # np.random.seed(0)
     # lidar = Lidar((0.01, 0.1), (0, 2*np.pi), 100, 4.9, BiasedPassage(num_walls=1))
-    # lidar = Lidar(0, (0, 2*np.pi), 100, 4.9, RandomSamplePassage(num_walls=3))
     lidar = Lidar(0,0,0,0)
 
     st = time.time()
