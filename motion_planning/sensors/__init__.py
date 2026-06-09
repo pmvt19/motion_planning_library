f09de90be05857b229d1db76d91271212ffd019e
pmvt19 <praval.telagi@gmail.com>
2026-06-08 19:58:05 -0700

    Adding public classes to interface

--- motion_planning/sensors/__init__.py
+++ motion_planning/sensors/__init__.py
@@ -1,3 +1,9 @@
 from .lidar import Lidar
 from .optimized_lidar import OptimizedLidar
 from .super_optimized_lidar import SuperOptimizedLidar
+
+__all__ = [
+    Lidar,
+    OptimizedLidar,
+    SuperOptimizedLidar
+]
